da6b22494e3471ce78fe875ebd9d95ae22e77e00
Tarun Pugalendi <tarunp.2795@gmail.com>
2024-04-06 20:07:50 +0530

    first commit

---
+++ src/test/java/com/example/azure/springazuredemo/SpringAzureDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.azure.springazuredemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAzureDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
